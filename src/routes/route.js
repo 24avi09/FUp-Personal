0da06ef555890f54bd3db9e34b9213ff99b77bb4
Abhishekh <3avu.yd@gmail.com>
2022-08-08 19:36:30 +0530

    lodash_session

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const lodash = require('lodash')
 const abc = require('../introduction/intro')
 const welcome = require('../logger/logger')
 const dateMon = require('../util/helper')
 const stringEdit = require('../validator/formatter')
+
 const router = express.Router();
 
 
@@ -16,6 +18,26 @@ router.get('/test-me', function (req, res) {
     stringEdit.trim()
     stringEdit.changetoLowerCase()
     stringEdit.changetoUpperCase()
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+    let arrayOfThree = lodash.chunk(monthNames,3)
+    console.log( "lodash.chunk is diving the above array in 4 sub-arrays and providing them in a array =>", arrayOfThree)
+    const firstTenOddNum = [1,3,5,7,9,11,13,15,17,19]
+    let lastNineNumOfFirstTenOddNum = lodash.tail(firstTenOddNum)
+    console.log( "lodash.tail is proving all the numbers except 1st number from the above array =>", lastNineNumOfFirstTenOddNum)
+    const arr1 = [1,2,3,4]
+    const arr2 = [3,4,5,6]
+    const arr3 = [1,4,5,7]
+    const arr4 = [2,6,7,9]
+    const arr5 = [1,5,7,11]
+    let union = lodash.union(arr1,arr2,arr3,arr4,arr5)
+    console.log( "lodash.union is giving the unique numbers from the above 5 arrays =>", union)
+    const ar1 = ["horror","The Shining"]
+    const ar2 = [ "drama","Titanic"] 
+    const ar3 = ["thriller","Shutter Island"]
+    const ar4 = ["fantasy","Pans Labyrinth"]
+    let pairs = lodash.fromPairs([ar1,ar2,ar3,ar4])
+    console.log( "lodash.fromPairs is pairing arrays into object =>",pairs)
 });
 
 
