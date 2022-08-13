ab10a66b93fb472328237344d1cf6f9eef6ba9ed
Abhishekh <3avu.yd@gmail.com>
2022-08-13 15:15:18 +0530

    assig_13_aug

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://AbhishekhYadav:Nf2J9z3RCavya8QU@cluster1.kwsn7az.mongodb.net/abhi03", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,8 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createNewBook", UserController.createBook )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBookList", UserController.getBookDetails)
 
 module.exports = router;
