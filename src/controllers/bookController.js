42f2ac5a5de52f900e284d8e6a38bd40921faf84
Abhishekh <3avu.yd@gmail.com>
2022-08-18 11:20:14 +0530

    assig done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,59 +1,68 @@
-const { count } = require("console")
-const BookModel= require("../models/bookModel")
+const { count } = require("console");
+const BookModel = require("../models/bookModel");
+const AuthorModel = require("../models/autherModel");
 
-const createBook= async function (req, res) {
-    let data= req.body
+const createBookList = async function (req, res) {
+  let data = req.body;
+  let savedBookData = await BookModel.create(data);
+  res.send({ msg: savedBookData });
+};
 
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
+
+
+const createAuthorList = async function (req, res) {
+  let data = req.body;
+  let savedAuthorData = await AuthorModel.create(data);
+  res.send({ msg: savedAuthorData });
+};
+
+
+
+const booksByChatenBhagat = async function (req, res) {
+  let authorIdOfChatenBhagat = await AuthorModel.findOne({
+    author_name: "Chetan Bhagat",
+  }).select({ author_id: 1, _id: 0 });
+  // console.log(authorIdOfChatenBhagat);
+  let bookByChatenBhagat = await BookModel.find(authorIdOfChatenBhagat);
+  // console.log(bookByChatenBhagat);
+  res.send({ msg: bookByChatenBhagat });
+};
+
+
+
+const updatedPrice_And_AuthorName = async function (req, res) {
+  const update = await BookModel.findOneAndUpdate(
+    { name: "Two states" },
+    { $set: { price: 100 } },
+    { new: true } // res send is updated 
+  );
+  let updatedPrice = await BookModel.findOne(update).select({
+    price: 1,
+    _id: 0,
+  });
+  // console.log(updatedPrice);
+  const authorName = await AuthorModel.findOne({
+    author_id: update.author_id,
+  }).select({ author_name: 1, _id: 0 });
+  // console.log(authorName);
+  res.send({ AuthorName_And_UpdatedPrice: authorName, updatedPrice });
+};
+
+
+
+const bookBetweenPrices = async function (req, res) {
+  const autherIdOfRange = await  BookModel.find( { price : { $gte: 50, $lte: 100} } ).select({ author_id :1, _id: 0})
+  // console.log(updat);
+  const arrOfAuthorId = autherIdOfRange.map(  x => x.author_id)
+  // console.log(arrOfAuthorId);
+  let authorNames =  await AuthorModel.find( {author_id : arrOfAuthorId}).select({author_name: 1, _id: 0})
+  // console.log(authorNames);
+  res.send({msg: authorNames});
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
-}
 
-
-const updateBooks= async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks= await BookModel.findOneAndUpdate( 
-        { authorName: "ABC"} , //condition
-        { $set: data }, //update in data
-        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
-     )
-     
-     res.send( { msg: allBooks})
-}
-
-const deleteBooks= async function (req, res) {
-    // let data = req.body 
-    let allBooks= await BookModel.updateMany( 
-        { authorName: "FI"} , //condition
-        { $set: {isDeleted: true} }, //update in data
-        { new: true } ,
-     )
-     
-     res.send( { msg: allBooks})
-}
-
-
-
-
-// CRUD OPERATIONS:
-// CREATE
-// READ
-// UPDATE
-// DELETE
-
-
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
+module.exports.createBookList = createBookList;
+module.exports.createAuthorList = createAuthorList;
+module.exports.booksByChatenBhagat = booksByChatenBhagat;
+module.exports.updatedPrice_And_AuthorName = updatedPrice_And_AuthorName;
+module.exports.bookBetweenPrices = bookBetweenPrices;
